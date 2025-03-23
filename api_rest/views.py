ddb32f172ebe33a91776205f9864023cbed86961
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2025-03-23 10:45:00 +0100

    feat - get all users

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+@api_view(['GET'])
+def get_users(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+
+        serializer = UserSerializer(users, many=true)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
