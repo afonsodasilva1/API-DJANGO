8c62681e41a8f3dbb1069676c6ccfa3844a662f2
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2025-03-23 11:26:03 +0100

    fix - couldn't get users

--- api_rest/views.py
+++ api_rest/views.py
@@ -16,8 +16,11 @@ import json
 def get_users(request):
 
     if request.method == 'GET':
+
         users = User.objects.all()
 
         serializer = UserSerializer(users, many=True)
+
         return Response(serializer.data)
+    
     return Response(status=status.HTTP_400_BAD_REQUEST)
